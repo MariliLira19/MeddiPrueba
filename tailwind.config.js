8b87a8367fad06a31871fe72601e2499baa47919
Marili Lira <marili_lira@itconsultants.com.mx>
2023-11-16 13:18:38 -0600

    Nuxt Project-extras

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
+
+module.exports = {
+  content: ["./pages/**/*.{vue,html,js}",
+  "./components/**/*.{vue,html,js}"],
+  theme: {
+    extend: {},
+
+    //Colores
+    
+
+
+  },
+  plugins: [],
+}
+
